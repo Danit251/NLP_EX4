13bd6ea5ce4e33d749261369bfc293cbd4e71c7a
danity <danity251@gmail.com>
2021-01-09 15:05:41 +0200

    fix bug in eval

--- eval.py
+++ eval.py
@@ -19,29 +19,29 @@ def process_data(path):
 if __name__ == '__main__':
     gold_relation = process_data(sys.argv[1])
     pred_relation = process_data(sys.argv[2])
-    total_pred = sum([len(v) for v in pred_relation.values()])
     total_gold = sum([len(v) for v in gold_relation.values()])
+    total_pred = sum([len(v) for v in pred_relation.values()])
     correct = 0
     examples_wrong = []
 
-    for sent_id, relation_list in pred_relation.items():
-        for (per_pred, org_pred, sentence) in relation_list:
+    for sent_id, pred_relation_list in pred_relation.items():
+        for (per_pred, org_pred, sentence) in pred_relation_list:
             suc = False
             if sent_id in gold_relation:
                 gold_wf_relation = gold_relation[sent_id]
-                for (per_gold, org_gold, _) in relation_list:
+                for (per_gold, org_gold, _) in gold_wf_relation:
                     if per_gold == per_pred and org_gold == org_pred:
                         suc = True
                         correct += 1
             if not suc:
                 examples_wrong.append((per_pred, org_pred, sentence))
 
-    precision = correct / total_pred
-    recall = correct / total_gold
-    f1 = (2 * precision * recall) / (precision + recall)
-    print(f"precision: {precision}")
-    print(f"recall: {recall}")
-    print(f"f1: {f1}")
+    precision = round((correct / total_pred) * 100, 2)
+    recall = round((correct / total_gold) * 100, 2)
+    f1 = round((2 * precision * recall) / (precision + recall), 2)
+    print(f"precision: {precision}%")
+    print(f"recall: {recall}%")
+    print(f"f1: {f1}%")
 
 
 
