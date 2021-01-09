c8072c3f08ba953d3f22a001fde39220640a0224
danity <danity251@gmail.com>
2021-01-09 18:02:49 +0200

    model eval

---
+++ model_eval.py
@@ -0,0 +1,32 @@
+from extract import load_from_pickle, predict, select_features, write_results, TRAIN_F, TEST_F, RelationsVectorizer, XGBClassifier
+from sklearn.metrics import classification_report, matthews_corrcoef
+
+train = load_from_pickle(TRAIN_F)
+test = load_from_pickle(TEST_F)
+
+vectorizer = RelationsVectorizer(train, test)
+# save_to_pickle(model, f"models/{model_name}.pkl")
+for ent_n in [50, 100, 200, 1000]:
+    for xgb in ["XGB", "RF"]:
+        model = XGBClassifier(n_estimators=100)
+        model.fit(vectorizer.train_vectors, vectorizer.train_labels)
+
+        predicted_labels = predict(model, vectorizer.test_vectors)
+
+        write_results(f"PRED.annotations_{str(ent_n)}_{xgb}.txt", test.op_relations, predicted_labels)
+
+        ranked_features = select_features(model, vectorizer.train_vectors, vectorizer.train_labels,
+                                          vectorizer.dv.feature_names_)
+        print(ranked_features)
+
+        report = classification_report(vectorizer.test_labels, predicted_labels)
+        print(report)
+        mcc = matthews_corrcoef(vectorizer.test_labels, predicted_labels)
+        print(f"MCC: {mcc}")
+        with open(f"models/report_{xgb}_{str(ent_n)}", "w") as f:
+            f.write(f"model name: {xgb}_{str(ent_n)}\n")
+            f.write(report)
+            f.write("\n~~~~~~~~~~\n")
+            f.write(f"MCC: {mcc}")
+            f.write("\n~~~~~~~~~~\n")
+            f.write(f"Rank: {ranked_features}")
