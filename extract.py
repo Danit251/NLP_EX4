b0feccd9f83ce4e28c0566d915ad7330daa04f04
danity <danity251@gmail.com>
2021-01-09 20:16:11 +0200

    remove dup from pred file

--- extract.py
+++ extract.py
@@ -314,10 +314,13 @@ def select_features(model, vectors, labels, features_names):
 
 
 def write_results(f_name, op_relations, predicted_labels):
+    rel_set = set()
     with open(f_name, "w") as f_res:
         for i, (idx, person, org, sentence) in enumerate(op_relations):
-            if predicted_labels[i] == "1":
+            rel_str = "_".join([idx, person[TEXT], RELATION, org[TEXT]])
+            if predicted_labels[i] == "1" and rel_str not in rel_set:
                 f_res.write("\t".join([idx, person[TEXT], RELATION, org[TEXT], f"( {sentence} )\n"]))
+                rel_set.add(rel_str)
 
 
 def main():
