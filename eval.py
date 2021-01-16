7c2b9344139f0ea35ac35f711730994657303f24
shaked571 <shakedgreenfeld@gmail.com>
2021-01-16 17:55:49 +0200

    jupyer notbook

--- eval.py
+++ eval.py
@@ -16,25 +16,42 @@ def process_data(path):
     return i2relations
 
 
+
+
+
 if __name__ == '__main__':
     gold_relation = process_data(sys.argv[1])
     pred_relation = process_data(sys.argv[2])
     total_gold = sum([len(v) for v in gold_relation.values()])
     total_pred = sum([len(v) for v in pred_relation.values()])
     correct = 0
-    examples_wrong = []
-
+    precision_mistakes = []
+    recall_mistakes = []
     for sent_id, pred_relation_list in pred_relation.items():
         for (per_pred, org_pred, sentence) in pred_relation_list:
             suc = False
             if sent_id in gold_relation:
-                gold_wf_relation = gold_relation[sent_id]
-                for (per_gold, org_gold, _) in gold_wf_relation:
+                pred_wf_relation = gold_relation[sent_id]
+                for (per_gold, org_gold, _) in pred_wf_relation:
                     if per_gold == per_pred and org_gold == org_pred:
                         suc = True
                         correct += 1
             if not suc:
-                examples_wrong.append((per_pred, org_pred, sentence))
+                precision_mistakes.append((sent_id, per_pred, org_pred, sentence))
+
+    for sent_id, pred_relation_list in gold_relation.items():
+        for (per_pred, org_pred, sentence) in pred_relation_list:
+            suc = False
+            if sent_id in pred_relation:
+                pred_wf_relation = pred_relation[sent_id]
+                for (per_gold, org_gold, _) in pred_wf_relation:
+                    if per_gold == per_pred and org_gold == org_pred:
+                        suc = True
+            if not suc:
+                recall_mistakes.append((sent_id, per_pred, org_pred, sentence))
+
+    # error_analysis_recall(recall_mistakes)
+
 
     precision = round((correct / total_pred) * 100, 2)
     recall = round((correct / total_gold) * 100, 2)
